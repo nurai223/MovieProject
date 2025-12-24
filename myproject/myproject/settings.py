95b948facb38c20c503bcd8a2c425e905301bdaf
nurai223 <nuraizhoomartkyzy@gmail.com>
2025-12-24 16:45:18 +0600

    njwb

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-from django.conf.global_settings import AUTH_USER_MODEL
 from dotenv import load_dotenv
 from datetime import timedelta
 
@@ -140,6 +139,7 @@ MODELTRANSLATION_LANGUAGES = ('en', 'ru')
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
